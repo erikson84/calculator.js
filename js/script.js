5c8798ac399f9f0cdf56ab5419fa29ae6e557ed0
Erikson Kaszubowski <erikson84@yahoo.com.br>
2022-08-16 23:32:05 -0300

    Implement clicl callback functions for numbers

--- js/script.js
+++ js/script.js
@@ -1,3 +1,48 @@
+const display = document.querySelector('.display');
+const numButtons = document.querySelectorAll(".button.number");
+const opsButtons = document.querySelectorAll(".button.operation");
+const ctrlButtons = document.querySelectorAll(".button.ctrl");
+
+const memory = {
+    a: null,
+    b: null,
+    operator: null,
+}
+
+numButtons.forEach(num => addEventListener('click', updateDisplay));
+opsButtons.forEach(num => addEventListener('click', storeNumOp));
+
+function updateDisplay(e) {
+    const curNum = display.textContent.trim();
+    const newNum = e.srcElement.textContent.trim();
+    if (curNum === '0') {
+        display.textContent = (newNum == '.') ? curNum + newNum : newNum;
+        return
+    }
+    if (newNum == '.') {
+        display.textContent = curNum.includes('.') ? curNum : curNum + newNum;
+        return
+    }
+    if (curNum.length > 10) {
+        display.textContent = curNum;
+        return
+    }
+
+    display.textContent = curNum + newNum;
+
+}
+
+function storeNumOp(e) {
+    if (memory.a === null) {
+        memory.a = +display.textContent;
+        memory.operator = e.srcElement.textContent;
+    } else {
+        
+    }
+
+}
+
+
 function add(a, b) {
     return a + b
 }
@@ -20,9 +65,9 @@ function operate(operator, a, b) {
             return add(a, b);
         case '-':
             return subtract(a, b);
-        case '*':
+        case '×':
             return multiply(a, b);
-        case '/':
+        case '÷':
             return multiply(a, b);
     }
 }
